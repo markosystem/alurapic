0ff6b9f218712709d728a94f150d45fedec64635
Marcos Batista <markos.system@gmail.com>
2019-11-07 01:29:29 -0300

    Correções

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'alurapic';
+
+  photos = [
+    {
+      url: 'https://ichef.bbci.co.uk/news/660/cpsprodpb/F767/production/_103953336_de27.jpg',
+      description: 'Leoa'
+    },
+    {
+      url: 'https://osegredo.com.br/wp-content/uploads/2018/07/signo-de-leao-830x450.jpg',
+      description: 'Leão'
+    }
+  ];
 }
