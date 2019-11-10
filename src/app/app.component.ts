c424967337e5051cd451b0e201285c79e06eea43
Marcos Batista <markos.system@gmail.com>
2019-11-10 17:58:32 -0300

    Correções e acompanhamento da aula

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'ap-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'alurapic';
-
-  photos = [
-    {
-      url: 'https://ichef.bbci.co.uk/news/660/cpsprodpb/F767/production/_103953336_de27.jpg',
-      description: 'Leoa'
-    },
-    {
-      url: 'https://osegredo.com.br/wp-content/uploads/2018/07/signo-de-leao-830x450.jpg',
-      description: 'Leão'
-    }
-  ];
 }
