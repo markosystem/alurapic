624c865823ac67ab35ff7c99d979f1bbc1a52e7e
Marcos Batista <markos.system@gmail.com>
2019-11-11 22:04:35 -0300

    Correções

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,8 +1,6 @@
-import { CardModule } from './../shared/components/card/card.module';
 import { NgModule } from '@angular/core';
 import { PhotoFormMudule } from './photo-form/photo-form.module';
 import { PhotoListModule } from './photo-list/photo-list.module';
-import { PhotoComponent } from './photo/photo.component';
 import { PhotoModule } from './photo/photo.module';
 
 
